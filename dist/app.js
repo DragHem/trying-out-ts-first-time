da67e7d432f442659f4b9a638029ca25dbb43cbf
Damian Puś <35109367+DragHem@users.noreply.github.com>
2022-05-04 19:01:33 +0200

    example 2

--- dist/app.js
+++ dist/app.js
@@ -22,12 +22,14 @@
 // test = true;
 // test = [];
 //Example 2
-const btn = document.querySelector("button");
-const calculatePrice = (orginalPrice, hasDiscount) => {
-    return hasDiscount ? orginalPrice * 0.8 : orginalPrice;
-};
-btn.addEventListener("click", () => {
-    const orginalPrice = 50;
-    const hasDiscount = new URLSearchParams(window.location.search).get("discount") === "true";
-    console.log(calculatePrice(orginalPrice, hasDiscount));
-});
+// const btn = document.querySelector("button");
+// const calculatePrice = (orginalPrice: number, hasDiscount: boolean) => {
+//   return hasDiscount ? orginalPrice * 0.8 : orginalPrice;
+// };
+// btn.addEventListener("click", () => {
+//   const orginalPrice: number = 50;
+//   const hasDiscount: boolean =
+//     new URLSearchParams(window.location.search).get("discount") === "true";
+//
+//   console.log(calculatePrice(orginalPrice, hasDiscount));
+// });
--- src/app.ts
+++ src/app.ts
@@ -24,14 +24,14 @@
 // test = [];
 
 //Example 2
-const btn = document.querySelector("button");
-const calculatePrice = (orginalPrice: number, hasDiscount: boolean) => {
-  return hasDiscount ? orginalPrice * 0.8 : orginalPrice;
-};
-btn.addEventListener("click", () => {
-  const orginalPrice: number = 50;
-  const hasDiscount: boolean =
-    new URLSearchParams(window.location.search).get("discount") === "true";
-
-  console.log(calculatePrice(orginalPrice, hasDiscount));
-});
+// const btn = document.querySelector("button");
+// const calculatePrice = (orginalPrice: number, hasDiscount: boolean) => {
+//   return hasDiscount ? orginalPrice * 0.8 : orginalPrice;
+// };
+// btn.addEventListener("click", () => {
+//   const orginalPrice: number = 50;
+//   const hasDiscount: boolean =
+//     new URLSearchParams(window.location.search).get("discount") === "true";
+//
+//   console.log(calculatePrice(orginalPrice, hasDiscount));
+// });
