48e0cd39929ec235b35269e085148cea64549111
Damian Puś <35109367+DragHem@users.noreply.github.com>
2022-05-04 11:47:34 +0200

    base types

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,9 @@
-console.log("test");
+let age = 22;
+age = 23;
+age = 31.5;
+let ageAsString = "dwadzieścia dwa";
+ageAsString = "dwanaście";
+ageAsString = `${age}`;
+console.log(ageAsString);
+const add = (v1, v2) => v1 + v2;
+console.log(add(10, 50));
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log("test");
+let age: number = 22;
+age = 23;
+age = 31.5;
+
+let ageAsString: string = "dwadzieścia dwa";
+ageAsString = "dwanaście";
+ageAsString = `${age}`;
+console.log(ageAsString);
+
+const add = (v1: number, v2: number) => v1 + v2;
+
+console.log(add(10, 50));
